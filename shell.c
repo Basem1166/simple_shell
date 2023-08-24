a2acfdd5073e95e4728bba64d868f3340608c1e7
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-24 15:56:22 -0700

    last ahmed testing

--- shell.c
+++ shell.c
@@ -1,6 +1,37 @@
 #include "shell.h"
 
 /**
+ * exuc_non - function that execute non-interacrive mode
+ * @line: buffer
+ * @line_size: buffer size
+ * @stdin: input
+ */
+void exuc_non(char **line, size_t *line_size, FILE *stream)
+{
+	ssize_t nread = getline(&line, &line_size, stdin);
+
+	if (nread == -1)
+		return;
+	if (checkbuiltins(check, line, nread) == 0)
+		return (exuc_non(&line, &line_size, stdin));
+	if (tokenize(nnread, argv, line) == 0)
+		return (exuc_non(&line, &line_size, stdin));
+	if (access(argv[0], X_OK) != 0)
+	{
+		if (nopath(command, argv) == 0)
+			return (exuc_non(&line, &line_size, stdin));
+	}
+	if (access(argv[0], X_OK) == 0)
+		forking(argv);
+	else
+	{
+		free(line);
+		perror(argv[0]);
+		 return (exuc_non(&line, &line_size, stdin));
+	}
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0.
@@ -15,9 +46,14 @@ int main(void)
 	char *argv[MAX];
 	char *nnread = NULL;
 
+	if (!isatty(STDIN_FILENO))
+	{
+		exuc_non(&line, &line_size, stdin);
+		free(line);
+		return (0);
+	}
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
 		write(1, "#cisfun$ ", 9);
 		nread = getline(&line, &line_size, stdin);
 
